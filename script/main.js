88c977bf495584cc84f17b4d51d1268e41351f4a
dchavezc <dervys75@gmail.com>
2017-03-21 21:10:09 -0400

    scrip et style ajutes

---
+++ script/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dchavezc on 2017-03-21.
+ */
+'use strict';
+/*Declaration de Constants*/
+const P_OPEN = '<p>';
+const P_CLOSE = '</p>';
+const M_OPEN = '<mark>';
+const M_CLOSE = '</mark>';
+
+/*Declaration de Variables*/
+
+
+/*Funtion*/
