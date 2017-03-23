b6bf873b4b3073bd0e00af77ab2539daa7b803f3
dchavezc <dervys75@gmail.com>
2017-03-23 09:18:09 -0400

    index coleur du site (-)

--- script/main_funtion.js
+++ script/main_funtion.js
@@ -17,32 +17,23 @@ const M_CLOSE = '</mark>';
 //
 // Apelle les Categories page INDEX
 
-// function charger_categories() {
-//     console.log('Executer charger_Categories');
-//     var categ_Forfaits = document.getElementById('list_categorie');
-//     categ_Forfaits.innerHTML = ""; // vider le ul
-//     // parcurrir les categories
-//     for (var i = 0; i < categories.length; i++) {
-//         categ_Forfaits.innerHTML += "<option>" + categories[i] + "</option>";
-//     }
-//
-// }
-// charger_categories();
-
-
-
-// // Apelle les Categories page CATALOGUE
-
-function  charger_forfait() {
-    console.log('Executer charger_forfait');
-    var listeForfaits = document.getElementById('listefotfaits');
-    listeForfaits.innerHTML = ""; // vider le ul
-    // parcurrir les forfaits
-    for (var i = 0; i < forfaits_data.length; i++) {
-        listeForfaits.innerHTML += "<li>" + P_OPEN + forfaits_data[i].nom + P_CLOSE + P_OPEN + '$' + forfaits_data[i].prix + P_CLOSE + P_OPEN + forfaits_data[i].ref_forfait + P_CLOSE + P_OPEN + forfaits_data[i].info_cat + P_CLOSE + '<img src="forfaits_data[i].img_catalogue">' +"</li>";
+function charger_categories() {
+    console.log('Executer charger_Categories');
+    var categ_Forfaits = document.getElementById('list_categorie');
+    categ_Forfaits.innerHTML = ""; // vider le ul
+    // parcurrir les categories
+    for (var i = 0; i < categories.length; i++) {
+        categ_Forfaits.innerHTML += "<option>" + categories[i] + "</option>";
     }
+
 }
-charger_forfait();
+charger_categories();
+
+
+
+
+
+
 
 
 // function faire_paragraphe(phrase) {
