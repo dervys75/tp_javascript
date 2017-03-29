6d42c3039a219b014c1846dc16183f0e2f275e76
sbociot <stevebocicot@icloud.com>
2017-03-29 09:08:32 -0400

    ‘Affiche du nom des categories’
    Signed-off-by: sbociot <stevebocicot@icloud.com>

--- script/catalogue.js
+++ script/catalogue.js
@@ -1,28 +1,21 @@
-/**
- * Created by dchavezc on 2017-03-23.
- */
-'use strict';
+"use strict";
 /*Declaration de Constants*/
-const P_OPEN = '<p>';
-const P_CLOSE = '</p>';
-const M_OPEN = '<mark>';
-const M_CLOSE = '</mark>';
 const IMG_PATH ="http://p86-tp-forfaits.projetisi.com/images/";
+var catalogueFilter;
+$(function(){
+	console.log('DOM Construit');
+	catalogueFilter = $('.main_catalogue_container');
+	console.log(catalogueFilter);
+	showCategories();
+// 	ShowForfaits();
+});
 
-/*Declaration de Variables*/
-
-
-/*Funtion*/
-
-// // Apelle les Categories page CATALOGUE
-
-function  charger_forfait() {
-    console.log('Executer charger_forfait');
-    var listeForfaits = document.getElementById('listefotfaits');
-    listeForfaits.innerHTML = ""; // vider le ul
-    // parcurrir les forfaits
-    for (var i = 0; i < forfaits_data.length; i++) {
-        listeForfaits.innerHTML += "<li>" + P_OPEN + forfaits_data[i].nom + P_CLOSE + P_OPEN + '$' + forfaits_data[i].prix + P_CLOSE + P_OPEN + forfaits_data[i].ref_forfait + P_CLOSE + P_OPEN + forfaits_data[i].info_cat + P_CLOSE + '<img src="'+IMG_PATH+forfaits_data[i].photo2+'">' +"</li>";
-    }
+function showCategories(){
+	console.log("Affiche les forfaits");
+	var titlesCat = catalogueFilter.children('h3.title_forfait');
+	console.log(titlesCat);
+	for(var i = 0; i < categories.length ; i++){
+		var typeCat = categories[i];
+		titlesCat.eq(i).text(typeCat);
+	}
 }
-charger_forfait();
