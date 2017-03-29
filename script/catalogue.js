3d9fce31475286c07344d953cc9889963921d1f2
sbociot <stevebocicot@icloud.com>
2017-03-29 12:53:16 -0400

    “modif des contenu”
    Signed-off-by: sbociot <stevebocicot@icloud.com>

--- script/catalogue.js
+++ script/catalogue.js
@@ -2,14 +2,16 @@
 /*Declaration de Constants*/
 const IMG_PATH ="http://p86-tp-forfaits.projetisi.com/images/";
 var catalogueFilter;
+var containerForfait;
 $(function(){
 	console.log('DOM Construit');
 	catalogueFilter = $('.main_catalogue_container');
+	containerForfait = $('.forfait_info');
 	console.log(catalogueFilter);
 	showCategories();
-// 	ShowForfaits();
+	ShowForfaits();
 });
-
+// Tous les titres de categorie des forfaits
 function showCategories(){
 	console.log("Affiche les forfaits");
 	var titlesCat = catalogueFilter.children('h3.title_forfait');
@@ -17,5 +19,18 @@ function showCategories(){
 	for(var i = 0; i < categories.length ; i++){
 		var typeCat = categories[i];
 		titlesCat.eq(i).text(typeCat);
+	}
+}
+
+function ShowForfaits() {
+	console.log("Afficher foratis",forfaits_data.length);
+	var theForfait = containerForfait.children('h4');
+// 	var image_forfait =containerForfait.find("table.wrapper_forfait tr.forfait_image");
+	console.log(theForfait);
+	for( var i = 0; i < forfaits_data.length ; i++){
+		var forfaits = forfaits_data[i];
+		theForfait.eq(forfaits.categorie).text(forfaits.nom);
+ 		/*image_forfait.eq(forfaits.categorie).append('<td><img src="'+IMG_PATH+forfaits_data[i].img_catalogue+ '" alt="'+ forfaits.nom+'"></td>');
+*/
 	}
 }
