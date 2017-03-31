aa350be4994b3440d440acc42984681e12646a78
sbociot <stevebocicot@icloud.com>
2017-03-31 09:42:18 -0400

    "structuration de la page catalogue"

--- script/catalogue.js
+++ script/catalogue.js
@@ -1,36 +1,37 @@
-"use strict";
-/*Declaration de Constants*/
+'use strict';
+var catalogueTitle;
+var showForfaits;
 const IMG_PATH ="http://p86-tp-forfaits.projetisi.com/images/";
-var catalogueFilter;
-var containerForfait;
-$(function(){
-	console.log('DOM Construit');
-	catalogueFilter = $('.main_catalogue_container');
-	containerForfait = $('.forfait_info');
-	console.log(catalogueFilter);
-	showCategories();
-	ShowForfaits();
-});
-// Tous les titres de categorie des forfaits
-function showCategories(){
-	console.log("Affiche les forfaits");
-	var titlesCat = catalogueFilter.children('h3.title_forfait');
-	console.log(titlesCat);
-	for(var i = 0; i < categories.length ; i++){
-		var typeCat = categories[i];
-		titlesCat.eq(i).text(typeCat);
-	}
-}
+$(function () {
+    console.log('DOM Construit');
+    catalogueTitle = $('#accordion_cats');
+    console.log(catalogueTitle);
+    showForfaits = $('.one_cat_forfaits')
+   requestTitle();
+   requestForfaitCat();
 
-function ShowForfaits() {
-	console.log("Afficher foratis",forfaits_data.length);
-	var theForfait = containerForfait.children('h4');
-// 	var image_forfait =containerForfait.find("table.wrapper_forfait tr.forfait_image");
-	console.log(theForfait);
-	for( var i = 0; i < forfaits_data.length ; i++){
-		var forfaits = forfaits_data[i];
-		theForfait.eq(forfaits.categorie).text(forfaits.nom);
- 		/*image_forfait.eq(forfaits.categorie).append('<td><img src="'+IMG_PATH+forfaits_data[i].img_catalogue+ '" alt="'+ forfaits.nom+'"></td>');
-*/
-	}
+});
+// function qui fait appel a les categories de forfaits
+function requestTitle() {
+  console.log('Appel de la function pour afficher les titres');
+  var titleContainer = catalogueTitle.children('h3');
+  console.log(titleContainer);
+  for(var i= 0; i < categories.length; i++ ){
+      var categorie = categories[i];
+      titleContainer.eq(i).text(categorie);
+  }
+}
+//function qui affiche les forfaits qui sont dans la base de données
+function  requestForfaitCat(){
+    console.log('Appel de la function pour afficher les forfaits');
+    var forfaitsContainer = showForfaits.find('.forfait_list');//--> le ul de class forfaits_list
+    console.log(forfaitsContainer);
+    for(var i = 0; i < forfaits_data.length; i++){
+        var forfaits = forfaits_data[i];
+        $('<li class="forfait">')
+            .appendTo(forfaitsContainer.eq(forfaits.categorie))
+            .append('<h4>' + forfaits.nom + '</h4>')
+            .append('<img src="'+IMG_PATH+forfaits_data[i].photo1+'"/>')
+            .append('<a href="reservation.html?forfid='+ i +'">Details Forfaits</a>');
+    }
 }
