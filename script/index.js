22cb8495b1de969508a66cd350b691bc61693ef9
sbociot <stevebocicot@icloud.com>
2017-04-02 16:28:14 -0400

    “script slider ajouter ”
    Signed-off-by: sbociot <stevebocicot@icloud.com>

--- script/index.js
+++ script/index.js
@@ -1,64 +1,68 @@
-/**
- * Created by dchavezc on 2017-03-20.
- */
-
 'use strict';
-
-
-/*Funtion*/
-//
-// Apelle les Categories page INDEX
-
-function color(elEvento){
-    console.log('Change couleur menu');
-   var evento = elEvento || window.event;
-   var tipo = evento.type;
-    if (tipo == "click") {
-        this.style.color = "whit";
-    }
-    else if (tipo == "dblclick") {
-        this.style.color = "olive";
-    }
-}
-window.onload = function() {
-    document.getElementsByClassName("colores").onclick = color;
-    document.getElementsByClassName("colores").ondblclick = color;
-};
-
-
-
-
-
-function charger_categories() {
-    console.log('Executer charger_Categories');
-    var categ_Forfaits = document.getElementById('list_categorie');
-    categ_Forfaits.innerHTML = ""; // vider le ul
-    // parcurrir les categories
-    for (var i = 0; i < categories.length; i++) {
-        categ_Forfaits.innerHTML += "<option>" + categories[i] + "</option>";
-    }
-
-}
-charger_categories();
-
-
-
-
-
-
-
-
-// function faire_paragraphe(phrase) {
-//     if ('undefined' == typeof phrase) { // Phrase pas fourni à l'appel
-//         phrase = ''; // Valeur par defaut
+// function color(elEvento){
+//     console.log('Change couleur menu');
+//    var evento = elEvento || window.event;
+//    var tipo = evento.type;
+//     if (tipo == "click") {
+//         this.style.color = "whit";
 //     }
-//     return P_OPEN + phrase + P_CLOSE;
+//     else if (tipo == "dblclick") {
+//         this.style.color = "olive";
+//     }
 // }
-//
-// var phrase;
-//
-// phrase = prompt("Veuillez ecrire une premiere phrase");
-// document.write(faire_paragraphe(phrase));
-//
-// phrase = prompt("Veuillez ecrire une deuxieme phrase");
-// document.write(faire_paragraphe(phrase));
+// window.onload = function() {
+//     document.getElementsByClassName("colores").onclick = color;
+//     document.getElementsByClassName("colores").ondblclick = color;
+// };
+
+
+
+
+
+// function charger_categories() {
+//     console.log('Executer charger_Categories');
+//     var categ_Forfaits = document.getElementById('list_categorie');
+//     categ_Forfaits.innerHTML = ""; // vider le ul
+//     // parcurrir les categories
+//     for (var i = 0; i < categories.length; i++) {
+//         categ_Forfaits.innerHTML += "<option>" + categories[i] + "</option>";
+//     }
+
+// }
+// charger_categories();
+
+$(function() {
+
+    //settings for slider
+    var width = 1150;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+});
