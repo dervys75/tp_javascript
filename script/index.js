1ad4196608419822a5656be3353b450e813f283d
sbociot <stevebocicot@icloud.com>
2017-04-03 12:49:45 -0400

    "forfait en promoi ajouiter"

--- script/index.js
+++ script/index.js
@@ -18,7 +18,7 @@
 
 
 
-
+const IMG_PATH ="http://p86-tp-forfaits.projetisi.com/images/";
 // function charger_categories() {
 //     console.log('Executer charger_Categories');
 //     var categ_Forfaits = document.getElementById('list_categorie');
@@ -34,9 +34,9 @@
 $(function() {
 
     //settings for slider
-    var width = 1150;
+    var width = 100 + '%';
     var animationSpeed = 1000;
-    var pause = 3000;
+    var pause = 2000;
     var currentSlide = 1;
 
     //cache DOM elements
@@ -64,7 +64,23 @@ $(function() {
         .on('mouseenter', pauseSlider)
         .on('mouseleave', startSlider);
 
-    startSlider();
+//   startSlider();
     // Forfait en promotion 
-    
+    console.log(forfaits_data[1].nom);
+    //les forfaits en promotion
+    $('<ul class="the_promo">').appendTo('.promo_forfaits');
+        $(".the_promo").append('<li><img src="'+IMG_PATH+forfaits_data[1].photo1+'"/>')
+        $(".the_promo li").append('<h3>'+forfaits_data[1].nom+'</h3>')
+            .append('<p>'+forfaits_data[1].debut_saison+'</p>')
+            .append('<p>$ '+forfaits_data[1].prix+'</p>');
+        $('<li>').appendTo('.the_promo');
+        $('.the_promo li').eq(1).append('<img src="'+IMG_PATH+forfaits_data[2].photo1+'"/>')
+            .append('<h3>'+forfaits_data[1].nom+'</h3>')
+            .append('<p>'+forfaits_data[1].debut_saison+'</p>')
+            .append('<p>$ '+forfaits_data[1].prix+'</p>');
+    $('<li>').appendTo('.the_promo');
+    $('.the_promo li').eq(2).append('<img src="'+IMG_PATH+forfaits_data[3].photo1+'"/>')
+        .append('<h3>'+forfaits_data[3].nom+'</h3>')
+        .append('<p>'+forfaits_data[3].debut_saison+'</p>')
+        .append('<p>$ '+forfaits_data[3].prix+'</p>');
 });
